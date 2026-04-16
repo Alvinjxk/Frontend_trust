25950096d017fbd3aa4fbcb258dd24e8a30b084d
Alvin <kyampwinealvin@gmail.com>
2026-04-16 06:57:16 +0300

    changed api

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: 'http://wed-production-f7410.up.railway.app/api/',
 });
 
 // Add token to every request
